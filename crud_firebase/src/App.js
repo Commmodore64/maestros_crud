297057500ccafed922816510c3cbaf4d076835f8
Cesar Marines <cesarml2002@hotmail.com>
2023-01-19 21:31:45 -0700

    Imprimiendo tabla con contenido de db

--- crud_firebase/src/App.js
+++ crud_firebase/src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
+// Importamos los componentes
 import Show from './components/Show';
+import Edit from './components/Edit';
+import Create from './components/Create';
+
+// Importamos el router
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <h1>HOLA</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={ <Show /> }/>
+          <Route path='/create' element={ <Create /> }/>
+          <Route path='/edit/:id' element={ <Edit /> }/>
+        </Routes>
+      </BrowserRouter>
 
-      <Show />
     </div>
   );
 }
--- crud_firebase/src/components/Show.js
+++ crud_firebase/src/components/Show.js
@@ -17,12 +17,22 @@ const Show = () => {
     // 3. Funcion para mostrar todos los docs
     const getMaestros = async () => {
         const data = await getDocs(maestrosCollection);
-        console.log(data.docs);
+        //console.log(data.docs);
+        setMaestros (
+          data.docs.map( (doc) => ( {...doc.data(), id:doc.id} ) )
+        )
+        console.log(maestros);
     }
 
     // 4. Funcion para eliminar un doc
+    const deleteMaestros = async (id) => {
+      const maestroDoc= doc(db, "maestros", id)
+      await deleteDoc(maestroDoc)
+      getMaestros()
+    }
 
     // 5. Funcion de configuracion para sweetalert2
+
 
     // 6. Usamos useEffect
     useEffect( () => {
@@ -32,7 +42,45 @@ const Show = () => {
 
     // 7. Devolvemos vista de nuestro componente
   return (
-    <div>show</div>
+    <>
+    <div className='container'>
+      <div className='row'>
+        <div className='col'>
+          <div className='d-grid gap-2'>
+          <Link to="/create" className='btn btn-secondary mt-2 mb-2'>Create</Link>
+          </div>
+
+          <table className='table table-dark table-hover'>
+            <thead>
+              <tr>
+                <th>Nombre</th>
+                <th>Curso</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              { maestros.map( (maestro)  => (
+                <tr key={maestro.id}>
+                  <td>{ maestro.nombre }</td>
+                  <td>{ maestro.curso }</td>
+                  
+                  <td>
+                    <Link to={`/edit/${maestro.id}`} className='btn btn-light'><i className="fa-solid fa-pen-to-square"></i></Link>
+                    <button onClick={ () => deleteMaestros(maestro.id)} className='btn btn-danger'><i className="fa-solid fa-trash"></i></button>
+                  </td>
+                </tr>
+
+              ))}
+                
+
+            </tbody>
+
+          </table>
+        </div>
+      </div>
+    </div>
+    </> 
   )
 }
 
